28988719a0c6c0637073641f9add545ab3c3a073
Mushaffar Khan <mushaffarkhan@Mushaffars-MacBook-Pro.local>
2017-07-20 16:26:28 -0400

    help me

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -1,44 +1,18 @@
-/*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-  http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
-
 package main
 
 import (
+    "errors"
     "fmt"
-    "strconv"
 
     "github.com/hyperledger/fabric/core/chaincode/shim"
-    pb "github.com/hyperledger/fabric/protos/peer"
 )
-
-// SimpleChaincode example simple Chaincode implementation
-type SimpleChaincode struct {
-}
-
-// ============================================================================================================================
-// Asset Definitions - The ledger will store marbles and owners
-// ============================================================================================================================
 
 // ----- Events ----- //
 type Event struct {
     ObjectType string        `json:"docType"` //field for couchdb
     Id       string          `json:"id"`      //the fieldtags are needed to keep case from bouncing around
     Amount      int           `json:"size"`    //size in mm of marble
-    Owner      OwnerRelation `json:"owner"`
+    Owner      Owner `json:"owner"`
 }
 
 // ----- Owners ----- //
@@ -48,72 +22,93 @@ type Owner struct {
     Username   string `json:"username"`
 }
 
-// ============================================================================================================================
-// Main
-// ============================================================================================================================
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
+}
+
 func main() {
     err := shim.Start(new(SimpleChaincode))
     if err != nil {
-        fmt.Printf("Error starting Simple chaincode - %s", err)
+        fmt.Printf("Error starting Simple chaincode: %s", err)
     }
 }
 
+// Init resets all the things
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+    if len(args) != 1 {
+        return nil, errors.New("Incorrect number of arguments. Expecting 1")
+    }
 
-// ============================================================================================================================
-// Init - initialize the chaincode - marbles don’t need anything initlization, so let's run a dead simple test instead
-// ============================================================================================================================
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-    fmt.Println("BlockPay is Starting Up")
-    _, args := stub.GetFunctionAndParameters()
-    var Aval int
+    err := stub.PutState("hello_world", []byte(args[0]))
+    if err != nil {
+        return nil, err
+    }
+
+    return nil, nil
+}
+
+// Invoke isur entry point to invoke a chaincode function
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+    fmt.Println("invoke is running " + function)
+
+    // Handle different functions
+    if function == "init" {
+        return t.Init(stub, "init", args)
+    } else if function == "write" {
+        return t.write(stub, args)
+    }
+    fmt.Println("invoke did not find func: " + function)
+
+    return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+// Query is our entry point for queries
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+    fmt.Println("query is running " + function)
+
+    // Handle different functions
+    if function == "read" { //read a variable
+        return t.read(stub, args)
+    }
+    fmt.Println("query did not find func: " + function)
+
+    return nil, errors.New("Received unknown function query: " + function)
+}
+
+// write - invoke function to write key/value pair
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    var key, value string
+    var err error
+    fmt.Println("running write()")
+
+    if len(args) != 2 {
+        return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+    }
+
+    key = args[0] //rename for funsies
+    value = args[1]
+    err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+    if err != nil {
+        return nil, err
+    }
+    return nil, nil
+}
+
+// read - query function to read key/value pair
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    var key, jsonResp string
     var err error
 
     if len(args) != 1 {
-        return shim.Error("Incorrect number of arguments. Expecting 1")
+        return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
     }
 
-    // convert numeric string to integer
-    Aval, err = strconv.Atoi(args[0])
+    key = args[0]
+    valAsbytes, err := stub.GetState(key)
     if err != nil {
-        return shim.Error("Expecting a numeric string argument to Init()")
+        jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+        return nil, errors.New(jsonResp)
     }
 
-    // store compaitible marbles application version
-    err = stub.PutState("marbles_ui", []byte("3.5.0"))
-    if err != nil {
-        return shim.Error(err.Error())
-    }
-
-    // this is a very simple dumb test.  let's write to the ledger and error on any errors
-    err = stub.PutState("selftest", []byte(strconv.Itoa(Aval))) //making a test var "selftest", its handy to read this right away to test the network
-    if err != nil {
-        return shim.Error(err.Error())                          //self-test fail
-    }
-
-    fmt.Println(" - ready for action")                          //self-test pass
-    return shim.Success(nil)
-}
-
-
-// ============================================================================================================================
-// Invoke - Our entry point for Invocations
-// ============================================================================================================================
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-    function, args := stub.GetFunctionAndParameters()
-    fmt.Println(" ")
-    fmt.Println("starting invoke, for - " + function)
-
-    // Handle different functions
-    if function == "init" {                    //initialize the chaincode state, used as reset
-        return t.Init(stub)
-    } else if function == "read" {             //generic read ledger
-        return read(stub, args)
-    } else if function == "write" {            //generic writes to ledger
-        return init_owner(stub, args)
-    }
-}
-
-    // error out
-    fmt.Println("Received unknown invoke function name - " + function)
-    return shim.Error("Received unknown invoke function name - '" + function + "'")
+    return valAsbytes, nil
 }
--- start/lib.go
+++ start/lib.go
@@ -1,20 +1,3 @@
-/*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-  http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
-
 package main
 
 import (
@@ -28,8 +11,8 @@ import (
 // ============================================================================================================================
 // Get Marble - get a marble asset from ledger
 // ============================================================================================================================
-func get_evnet(stub shim.ChaincodeStubInterface, id string) (Event, error) {
-    var event Event 
+func get_event(stub shim.ChaincodeStubInterface, id string) (Event, error) {
+    var event Event
     eventAsBytes, err := stub.GetState(id)                  //getState retreives a key/value from the ledger
     if err != nil {                                          //this seems to always succeed, even if key didn't exist
         return event, errors.New("Failed to find marble - " + id)
